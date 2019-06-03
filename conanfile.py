9bf50c190e6545f7496b0591ed99e597f939a936
Didier WECKMANN <didier.weckmann@ircad.fr>
2019-06-03 14:17:49 +0200

    feat(conanfile): update glog from 0.4.0 to 0.4.0-r1

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ import shutil
 
 class LibGlogConan(ConanFile):
     name = "glog"
-    package_revision = ""
+    package_revision = "-r1"
     upstream_version = "0.4.0"
     version = "{0}{1}".format(upstream_version, package_revision)
     generators = "cmake"
@@ -25,15 +25,25 @@ class LibGlogConan(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
 
+    def requirements(self):
+        self.requires("common/1.0.0@sight/stable")
+
     def source(self):
         tools.get("https://github.com/google/glog/archive/v{0}.tar.gz".format(self.upstream_version))
         os.rename("glog-" + self.upstream_version, self.source_subfolder)
 
     def build(self):
+        #Import common flags and defines
+        import common
         glog_source_dir = os.path.join(self.source_folder, self.source_subfolder)
         shutil.move("patches/CMakeProjectWrapper.txt", "CMakeLists.txt")
 
         cmake = CMake(self)
+        
+        #Set common flags
+        cmake.definitions["CMAKE_C_FLAGS"] = common.get_c_flags()
+        cmake.definitions["CMAKE_CXX_FLAGS"] = common.get_cxx_flags()
+        
         cmake.definitions["BUILD_TESTING"] = "OFF"
         cmake.definitions["WITH_GFLAGS"] = "OFF"
         if not tools.os_info.is_windows:
